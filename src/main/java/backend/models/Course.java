a5f141897eed166d4721f17cc598e521e1a21e85
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-11 15:00:22 +0530

    Add course model

---
+++ src/main/java/backend/models/Course.java
@@ -0,0 +1,27 @@
+package backend.models;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course(String courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
