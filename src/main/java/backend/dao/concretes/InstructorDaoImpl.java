e8eee35612563e3cefd55fb8b35b610a89a0a6eb
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-11 19:42:35 +0530

    Add instructor dao implementation

---
+++ src/main/java/backend/dao/concretes/InstructorDaoImpl.java
@@ -0,0 +1,28 @@
+package backend.dao.concretes;
+
+import backend.dao.interfaces.InstructorDao;
+import backend.models.Instructor;
+
+import java.util.List;
+
+public class InstructorDaoImpl implements InstructorDao <Instructor> {
+    public List<Instructor> getAllInstructors() {
+        return null;
+    }
+
+    public void addInstructor(Instructor instructor) {
+
+    }
+
+    public Instructor getOneInstructor() {
+        return null;
+    }
+
+    public void updateInstructor(Instructor instructor) {
+
+    }
+
+    public void deleteInstructor(String nic) {
+
+    }
+}
