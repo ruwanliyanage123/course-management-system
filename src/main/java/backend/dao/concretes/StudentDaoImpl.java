0a04eb7f3a57bca2a63040dc9c857a101de62675
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-11 19:44:46 +0530

    Add student dao implementation

---
+++ src/main/java/backend/dao/concretes/StudentDaoImpl.java
@@ -0,0 +1,28 @@
+package backend.dao.concretes;
+
+import backend.dao.interfaces.StudentDao;
+import backend.models.Student;
+
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao<Student> {
+    public List<Student> getAllStudents() {
+        return null;
+    }
+
+    public void addStudent(Student student) {
+
+    }
+
+    public Student getOneStudent() {
+        return null;
+    }
+
+    public void updateStudent(Student student) {
+
+    }
+
+    public void deleteStudent(int studentId) {
+
+    }
+}
