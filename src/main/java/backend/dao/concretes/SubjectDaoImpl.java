f6700b1be4c8ac9b3494630dcb965e48549851fd
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-11 19:48:53 +0530

    Add subject dao implementation

---
+++ src/main/java/backend/dao/concretes/SubjectDaoImpl.java
@@ -0,0 +1,28 @@
+package backend.dao.concretes;
+
+import backend.dao.interfaces.SubjectDao;
+import backend.models.Subject;
+
+import java.util.List;
+
+public class SubjectDaoImpl implements SubjectDao<Subject> {
+    public List<Subject> getAllSubjects() {
+        return null;
+    }
+
+    public void addSubject(Subject subject) {
+
+    }
+
+    public Subject getOneSubject() {
+        return null;
+    }
+
+    public void updateSubject(Subject subject) {
+
+    }
+
+    public void deleteSubject(String subjectId) {
+
+    }
+}
