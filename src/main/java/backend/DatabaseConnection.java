e991d30f65b90c849805df35691b41345bbe177b
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-12 09:57:40 +0530

    Add slf4j logger

--- src/main/java/backend/DatabaseConnection.java
+++ src/main/java/backend/DatabaseConnection.java
@@ -3,16 +3,19 @@ package backend;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class used the Singleton design pattern to create and distribute the database connection
  */
 public class DatabaseConnection {
+    private static Logger logger = LoggerFactory.getLogger(DatabaseConnection.class);
     private static DatabaseConnection databaseConnection;
     private Connection connection;
-    private String hostUrl;
-    private String username;
-    private String password;
+    private String hostUrl ="";
+    private String username="";
+    private String password="";
 
     /**
      * we have to keep constructor as private to limit the class for only one instance.
@@ -20,23 +23,25 @@ public class DatabaseConnection {
     private DatabaseConnection() {
         try {
             Class.forName("");
-            assert hostUrl != null;
+            assert false;
             this.connection = DriverManager.getConnection(hostUrl, username, password);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            String message = "Check JDBC connection";
+            logger.error(message,e);
         } catch (SQLException e) {
-            e.printStackTrace();
+            String message = "Check host, username, password in MySQL connection";
+            logger.error(message,e);
         }
     }
 
-    public Connection getConnection() {
+    private Connection getConnection() {
         return connection;
     }
 
     /**
      * By using static method, no need to create an instance. instead of that can direct call the method and get the connection
      * @return created database connection
-     * @throws SQLException
+     * @throws SQLException can be arise, if connection will null
      */
     public static DatabaseConnection getInstance() throws SQLException {
         if (databaseConnection == null) {
