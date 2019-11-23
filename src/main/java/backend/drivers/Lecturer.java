3a8e80620adee92bed4ea2eb990a874584dfe88b
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-23 21:50:59 +0530

    Add lecture ui

---
+++ src/main/java/backend/drivers/Lecturer.java
@@ -0,0 +1,24 @@
+package backend.drivers;
+
+import javax.swing.*;
+
+public class Lecturer {
+
+    private JPanel lecturer;
+    private JTextField nic;
+    private JTextField lecturerHours;
+    private JTextField firstName;
+    private JTextField street;
+    private JTextField lastName;
+    private JTextField city;
+    private JTextField mobile;
+    private JTextField salary;
+    private JTextField email;
+    private JButton addButton;
+    private JButton viewButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton editSubmitButton;
+    private JButton clearButton;
+    private JLabel firstNam;
+}
