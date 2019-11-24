ca7748ca48a8e0e9ad92fe746b06060bdc8991e0
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-24 09:13:58 +0530

    Add add button listener

--- src/main/java/backend/drivers/InstructorUI.java
+++ src/main/java/backend/drivers/InstructorUI.java
@@ -1,13 +1,19 @@
 package backend.drivers;
 
+import backend.dao.concretes.InstructorDaoImpl;
+import backend.dao.interfaces.InstructorDao;
+import backend.models.Instructor;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InstructorUI {
     private JPanel instructor;
     private JTextField nic;
-    private JTextField lecturerHours;
+    private JTextField practicalHours;
     private JTextField street;
-    private JTextField fisrtName;
+    private JTextField firstName;
     private JTextField lastName;
     private JTextField city;
     private JTextField mobile;
@@ -19,4 +25,29 @@ public class InstructorUI {
     private JButton deleteButton;
     private JButton editSubmitButton;
     private JButton clearButton;
+
+    public InstructorUI() {
+
+        /**
+         * to add a instructor
+         */
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nic1 = nic.getText();
+                String firstName1 = firstName.getText();
+                String lastName1 = lastName.getText();
+                String mobile1 = mobile.getText();
+                String email1 = email.getText();
+                double salary1 = Double.parseDouble(salary.getText());
+                String city1 = city.getText();
+                String street1 = street.getText();
+                int practicalHours1 = Integer.parseInt(practicalHours.getText());
+                Instructor instructor = new Instructor(nic1,firstName1, lastName1, city1, street1, email1, mobile1, salary1);
+                instructor.setWorkingHours(practicalHours1);
+                InstructorDao instructorDao = new InstructorDaoImpl();
+                instructorDao.addInstructor(instructor);
+            }
+        });
+    }
 }
