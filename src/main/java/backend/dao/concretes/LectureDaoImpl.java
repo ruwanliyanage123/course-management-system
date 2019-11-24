b5381949ffee64cf0f7e76c6bd3377ba44305f23
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-11-24 08:59:57 +0530

    Add instructor ui

--- src/main/java/backend/dao/concretes/LectureDaoImpl.java
+++ src/main/java/backend/dao/concretes/LectureDaoImpl.java
@@ -171,3 +171,4 @@ public class LectureDaoImpl implements LectureDao<Lecture> {
         }
     }
 }
+
---
+++ src/main/java/backend/drivers/InstructorUI.java
@@ -0,0 +1,22 @@
+package backend.drivers;
+
+import javax.swing.*;
+
+public class InstructorUI {
+    private JPanel instructor;
+    private JTextField nic;
+    private JTextField lecturerHours;
+    private JTextField street;
+    private JTextField fisrtName;
+    private JTextField lastName;
+    private JTextField city;
+    private JTextField mobile;
+    private JTextField email;
+    private JTextField salary;
+    private JButton addButton;
+    private JButton viewButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton editSubmitButton;
+    private JButton clearButton;
+}
