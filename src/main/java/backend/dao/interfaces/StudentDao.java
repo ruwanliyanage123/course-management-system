17209c22e12dc016e6deaf1e991ae59fd52e2fe5
ruwan liyanage <ruwanliyanagegalle1994@gmail.com>
2023-01-11 23:54:51 +0530

    Update StudentDao.java

--- src/main/java/backend/dao/interfaces/StudentDao.java
+++ src/main/java/backend/dao/interfaces/StudentDao.java
@@ -2,6 +2,9 @@ package backend.dao.interfaces;
 
 public interface StudentDao<Student> {
 
+    /**
+    *No need to add the public abstract because those are default keywords for the interface methods 
+    */
     public abstract String[][] getAllStudents();
 
     public abstract void addStudent(Student student);
